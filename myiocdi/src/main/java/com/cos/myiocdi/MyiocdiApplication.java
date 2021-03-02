1eea09367809b8fee6b9047c1341c0c0fd0c6e81
uheaji <yoolbk@naver.com>
2021-03-02 12:38:00 +0900

    1. 컨트롤러 제어 방법

---
+++ myiocdi/src/main/java/com/cos/myiocdi/MyiocdiApplication.java
@@ -0,0 +1,13 @@
+package com.cos.myiocdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyiocdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyiocdiApplication.class, args);
+	}
+
+}
---
+++ myiocdi/src/main/java/com/cos/myiocdi/Robot.java
@@ -0,0 +1,10 @@
+package com.cos.myiocdi;
+
+import lombok.Getter;
+
+@Getter
+public class Robot {
+	
+	private String name = "건담";
+
+}
